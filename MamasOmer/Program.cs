f79b9b62de46fef34c0ebe937dc1a089e9d19af7
noach844 <noach844@gmail.com>
2021-06-23 09:58:16 +0300

    basic setup

--- MamasOmer/Program.cs
+++ MamasOmer/Program.cs
@@ -3,10 +3,10 @@
 namespace MamasOmer
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
-        {            
-            Console.WriteLine("Hello World!");            
+        {
+            
         }
     }
 }
