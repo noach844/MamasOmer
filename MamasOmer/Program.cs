aa384461b67f1c62ac22eafd61783f7eb143a345
noach844 <68166931+noach844@users.noreply.github.com>
2021-06-24 10:55:28 +0300

    finished config serializer

--- MamasOmer/Program.cs
+++ MamasOmer/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Configuration;
 using MamasOmer.Classes;
+using MamasOmer.Classes.Exceptions;
 
 namespace MamasOmer
 {
     class Program
     {        
         static void Main(string[] args)
-        {
-            RollsInitializer test = new RollsInitializer();
+        {           
         }
     }
 }
